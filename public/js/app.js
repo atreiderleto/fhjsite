6952376445349c697a7b6f354ae84bbe60fd8b75
Alberto <atreider@protonmail.com>
2021-02-12 10:27:29 -0500

    Revision

---
+++ public/js/app.js
@@ -0,0 +1,64 @@
+//selectores del menu lateral izquierdo
+const menuL = document.querySelector('#menul');
+const cl = document.querySelector('#cerrarl')
+
+//selectores del menu lateral derecho 
+const menuR = document.querySelector('.menumobil');
+const menuBtn = document.querySelector('.menu-btn');
+const cr = document.querySelector('.cerrar-l');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    menuL.addEventListener('click', openL);
+    cl.addEventListener('click', closeL);
+
+    menuR.addEventListener('click', openR);
+    menuBtn.addEventListener('click', openRD);
+    cr.addEventListener('click', closeR);
+});
+
+//abre el menu lateral izquierdo en mobil y tablet
+const openL = () => {
+    
+    const latertalL = document.querySelector('.lateral-l');
+    latertalL.classList.remove('hidden')
+    
+}
+
+//cierrar el menu lateral izquierdo en mobil y tablet
+
+const closeL = () => {
+    
+    const latertalL = document.querySelector('.lateral-l');
+    latertalL.classList.add('hidden')
+    
+}
+
+//abre el menu lateral Derecho 
+
+const openR = () => {
+    
+    const latertalR = document.querySelector('.lateral-r');
+    latertalR.classList.remove('hidden')
+    return;
+}
+
+
+//abre el menu lateral Derecho  versino escritorio
+
+const openRD = () => {
+    
+    const latertalR = document.querySelector('.lateral-r');
+    latertalR.classList.remove('hidden')
+    return;
+}
+
+
+//cierrar el menu lateral derecho 
+
+const closeR = () => {
+    
+    const latertalR = document.querySelector('.lateral-r');
+    latertalR.classList.add('hidden')
+    return;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  purge: [
+    './src/**/*.html',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        quick: ['Quicksand'],
+      },
+      colors: {
+        bgf: '#F7F7FC',
+        sblue: "#002B52",
+        syellow: '#FFCE00',
+      },
+      backgroundImage: theme => ({
+        'busqueda': "url('../public/img/medio/icons8_search.svg')",
+        'jumbo': "url('../public/img/medio/jumbotron.png')",
+        'sticker': "url('../public/img/medio/grupo3.png')",
+        'sticker1': "url('../public/img/medio/Group4.svg')",
+        'sticker2': "url('../public/img/medio/Group5.svg')",
+        'cursos': "url('../public/img/cursos/Rectangle631.png')",
+      }),
+      keyframes: {
+
+        menul: {
+
+          '0%': { transform: 'translateX(-100%)' },
+          '50%': { transform: 'translateX(0.5)' },
+          '100%': { transform: 'translateX(1)' }
+        },
+
+        menur: {
+
+          '0%': { transform: 'translateX(100%)' },
+          '50%': { transform: 'translateX(0.5)' },
+          '100%': { transform: 'translateX(1)' }
+        }
+      },
+
+      animation: {
+        menur: 'menur 600ms',
+        menul: 'menul 600ms'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
