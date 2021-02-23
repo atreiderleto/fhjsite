186e1d98cd87aec518f3980d9b49b0bbad543b88
Alberto <atreider@protonmail.com>
2021-02-23 08:55:14 -0500

    Revision

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,12 +14,12 @@ module.exports = {
         syellow: '#FFCE00',
       },
       backgroundImage: theme => ({
-        'busqueda': "url('/src/img/medio/icons8_search.svg')",
-        'jumbo': "url('/src/img/medio/jumbotron.png')",
-        'sticker': "url('/src/img/medio/grupo3.png')",
-        'sticker1': "url('/src/img/medio/Group4.svg')",
-        'sticker2': "url('/src/img/medio/Group5.svg')",
-        'cursos': "url('/src/img/cursos/Rectangle631.png')",
+        'busqueda': "url('/public/img/medio/icons8_search.svg')",
+        'jumbo': "url('/public/img/medio/jumbotron.png')",
+        'sticker': "url('/public/img/medio/grupo3.png')",
+        'sticker1': "url('/public/img/medio/Group4.svg')",
+        'sticker2': "url('/public/img/medio/Group5.svg')",
+        'cursos': "url('/public/img/cursos/Rectangle631.png')",
       }),
       keyframes: {
 
